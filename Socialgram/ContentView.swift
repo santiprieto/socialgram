cb29eede16d3e7e77bebf5264e6b36268915b8ab
Santiago Prieto <80703690+santiprieto@users.noreply.github.com>
2025-02-01 12:51:59 -0300

    xcode project

---
+++ Socialgram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Socialgram
+//
+//  Created by Santiago Prieto on 1/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
